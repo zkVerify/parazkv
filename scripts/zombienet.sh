4e3e181147c45b35515b75dbc4138a127377246d
DimitrisPapac <dimpapach87@gmail.com>
2025-11-10 21:31:54 +0100

    Checkpoint

---
+++ scripts/zombienet.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+ZOMBIENET_V=v1.3.128
+BIN_DIR=relay-bin
+CHAIN=${CHAIN:-"local"}
+
+case "$(uname -s)" in
+    Linux*)     MACHINE=Linux;;
+    Darwin*)    MACHINE=Mac;;
+    *)          exit 1
+esac
+
+arch="$(uname -m)"
+
+if [ $MACHINE = "Linux" ]; then
+  if [ "$arch" == "arm64" ] || [ "$arch" == "aarch64" ]; then
+        ZOMBIENET_BIN="zombienet-linux-arm64"
+    else
+        ZOMBIENET_BIN="zombienet-linux-x64"
+    fi
+elif [ $MACHINE = "Mac" ]; then
+  if [ "$arch" == "arm64" ] || [ "$arch" == "aarch64" ]; then
+        ZOMBIENET_BIN="zombienet-macos-arm64"
+    else
+        ZOMBIENET_BIN="zombienet-macos-x64"
+    fi
+fi
+
+
+
+build_zkVerify() {
+  echo "cloning zkVerify repository..."
+  CWD=$(pwd)
+  mkdir -p "$BIN_DIR"
+  pushd /tmp
+    git clone https://github.com/zkVerify/zkVerify.git
+    pushd zkVerify
+      # Get new tags from remote
+      git fetch --tags > /dev/null
+      # Get latest tag name
+      latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+      # Checkout latest tag
+      git checkout $latestTag
+      echo "building zkVerify executable..."
+      cargo build --release -p zkv-relay --features "fast-runtime"
+      cp target/release/zkv-relay "$CWD/$BIN_DIR"
+      cp target/release/zkv-relay-execute-worker "$CWD/$BIN_DIR"
+      cp target/release/zkv-relay-prepare-worker "$CWD/$BIN_DIR"
+    popd
+  popd
+}
+
+zombienet_init() {
+  if [ ! -f $ZOMBIENET_BIN ]; then
+    echo "fetching zombienet executable..."
+    curl -LO https://github.com/paritytech/zombienet/releases/download/$ZOMBIENET_V/$ZOMBIENET_BIN
+    chmod +x $ZOMBIENET_BIN
+  fi
+  if [ ! -f $BIN_DIR/zkv-relay ]; then
+   echo "Fetch_zkVerify() not yet implemented: you must execute 'zombienet build'"
+  fi
+  if [ ! -f $BIN_DIR/zkv-relay-execute-worker ] || [ ! -f $BIN_DIR/zkv-relay-prepare-worker ]; then
+   echo "The zkVerify node binary is available, but external workers are missing."
+   exit -1
+  fi
+}
+
+zombienet_build() {
+  if [ ! -f $ZOMBIENET_BIN ]; then
+    echo "fetching zombienet executable..."
+    curl -LO https://github.com/paritytech/zombienet/releases/download/$ZOMBIENET_V/$ZOMBIENET_BIN
+    chmod +x $ZOMBIENET_BIN
+  fi
+  if [ ! -f $BIN_DIR/zkv-relay ]; then
+    build_zkVerify
+  fi
+}
+
+zombienet_devnet() {
+  zombienet_init
+  # cargo build --release
+  echo "spawning local relay chain plus devnet ${CHAIN} as a parachain..."
+  local dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  ./$ZOMBIENET_BIN spawn "$dir/../zombienet-config/devnet-${CHAIN}.toml" -p native
+}
+
+
+zombienet_debug() {
+  zombienet_init
+  echo "compiling parachain node"
+  cargo build
+  echo "spawning local relay chain plus devnet as a parachain..."
+  local dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  ./$ZOMBIENET_BIN spawn "$dir/../zombienet-config/debug.toml" -p native
+}
+
+
+print_help() {
+  echo "This is a shell script to automate the execution of zombienet."
+  echo ""
+  echo "$ ./zombienet.sh init         # fetches zombienet and zkVerify executables"
+  echo "$ ./zombienet.sh build        # builds zkVerify executables from source"
+  echo "$ ./zombienet.sh devnet       # spawns a local relay chain plus parachain devnet-local as a parachain"
+}
+
+SUBCOMMAND=$1
+case $SUBCOMMAND in
+  "" | "-h" | "--help")
+    print_help
+    ;;
+  *)
+    shift
+    zombienet_${SUBCOMMAND} $@
+    if [ $? = 127 ]; then
+      echo "Error: '$SUBCOMMAND' is not a known SUBCOMMAND." >&2
+      echo "Run './zombienet.sh --help' for a list of known subcommands." >&2
+        exit 1
+    fi
+  ;;
+esac
---
+++ zombienet-tests/test_runner.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+# This script is used to run the zombienet tests locally or in the CI pipeline.
+# If run locally, be sure that the following applications are present on
+# the target system:
+# - node
+# - npm
+# - yarn
+# The script automatically downloads zombienet binary and saves it into the zombienet-tests/bin folder.
+# It also looks for a compiled zkv-relay binary in the folder target/release, hence make sure to 
+# have a freshly compiled version of zkv-relay in this folder.
+# Optionally, this script can be launched with the '--debug' switch, which makes it look for
+# the zkv-relay binary in the target/debug folder instead.
+
+# ANSI color handles
+TXT_BIBLU="\033[94;1m"
+TXT_BIYLW="\033[93;1m"
+TXT_BIGRN="\033[92;1m"
+TXT_BIRED="\033[91;1m"
+TXT_BIBLK="\033[90;1m"
+TXT_NORML="\033[0m"
+
+# The return value of each zombienet invocation is always equal to the
+# number of failed tests among those listed in each .zndsl.
+# For this reason, we keep track of each .zndsl whose return value is not 0.
+FAILED_TESTS=()
+TOT_EXEC_TESTS=0
+TOT_FAIL_TESTS=0
+EXIT_STATUS=0
+
+# Directory for zkv-relay binary.
+BIN_DIR=relay-bin
+
+# Check operating system and set variables for binary name
+OS="$(uname)"
+ARCH="$(uname -m)"
+# zombienet v1.3.128 provides @polkadot/api: "^14.3.1",
+BASE_URL="https://github.com/paritytech/zombienet/releases/download/v1.3.128"
+case "$OS" in
+    "Linux")
+        case "$ARCH" in
+            "x86_64") ZOMBIENET_BINARY="zombienet-linux-x64" ;;
+            "aarch64") ZOMBIENET_BINARY="zombienet-linux-arm64" ;;
+            *)
+                echo -e "${TXT_BIRED}ERROR: ${TXT_BIBLK}Unsupported architecture on Linux: $ARCH${TXT_NORML}"
+                exit 4
+                ;;
+        esac
+        ;;
+    "Darwin")
+        case "$ARCH" in
+            "x86_64") ZOMBIENET_BINARY="zombienet-macos-x64" ;;
+            "arm64") ZOMBIENET_BINARY="zombienet-macos-arm64" ;;
+            *)
+                echo -e "${TXT_BIRED}ERROR: ${TXT_BIBLK}Unsupported architecture on macOS: $ARCH${TXT_NORML}"
+                exit 4
+                ;;
+        esac
+        ;;
+    *)
+        echo -e "${TXT_BIRED}ERROR: ${TXT_BIBLK}Unsupported operating system: $OS${TXT_NORML}"
+        exit 4
+        ;;
+esac
+
+ZOMBIENET_URL="${BASE_URL}/${ZOMBIENET_BINARY}"
+
+# Check if Zombienet executable exists, otherwise download it
+if [ ! -f "bin/$ZOMBIENET_BINARY" ]; then
+    mkdir -p bin
+    echo -e "${TXT_BIYLW}WARNING: ${TXT_BIBLK}Zombienet executable not found${TXT_NORML}"
+    curl -s -L $ZOMBIENET_URL -o "bin/$ZOMBIENET_BINARY"
+    if [ $? -ne 0 ]; then
+        echo -e "${TXT_BIRED}ERROR: ${TXT_BIBLK}Failed to download Zombienet binary.${TXT_NORML}"
+        exit 2
+    fi
+    chmod +x "bin/$ZOMBIENET_BINARY"
+fi
+
+declare -a TEST_LIST=()
+
+# Check if we requested a run over a debug build
+PROFILE="release"
+for ARG in "$@"; do
+    if [[ "${ARG}" == "--debug" ]]; then
+        PROFILE="debug"
+    else
+        TEST_LIST+=("${ARG}")    
+    fi
+done
+
+if [ ${#TEST_LIST[@]} -eq 0 ]; then
+    # Please do not exceed 64 chars for each test filename - including the .zndsl extension
+    IFS=$'\n' TEST_LIST=($(find . -name "*.zndsl" | sort))
+fi
+
+echo -e "${TXT_BIGRN}INFO: ${TXT_BIBLK}Running tests with a ${PROFILE} build${TXT_NORML}"
+
+HAS_BINARIES="true"
+if [[ ! ( -f "../${BIN_DIR}/zkv-relay" && -f "../target/${PROFILE}/paratest-node" ) ]]; then
+    echo -e "${TXT_BIRED}ERROR: ${TXT_BIBLK}zkv-relay and/or paratest-node binary not found. Compile paratest-node in ${PROFILE} mode and re-launch this script${TXT_NORML}"
+    echo -e "       ${TXT_BIBLK}cargo build -p paratest-node --${PROFILE} --features fast-runtime${TXT_NORML}"
+    HAS_BINARIES="false"
+fi
+
+if [[ ${HAS_BINARIES} == "false" ]]; then
+    exit 2
+fi
+
+# If all checks passed, set the full build path
+FULLBUILDPATH="../target/${PROFILE}:../${BIN_DIR}"
+
+# GO! GO! GO!
+echo -e "============================================================"
+echo -e "${TXT_BIBLK} TEST NODE impl:  ${TXT_NORML} ${NODE}"
+echo -e "============================================================"
+for TESTNAME in "${TEST_LIST[@]}"; do
+    echo -e "\n\n"
+    echo -e "============================================================"
+    echo -e "${TXT_BIBLK} Running test:  ${TXT_NORML} ${TESTNAME} on ${NODE} chain"
+    echo -e "============================================================"
+    ( PATH="${PATH}:${FULLBUILDPATH}"; bin/$ZOMBIENET_BINARY -p native test ./"${TESTNAME}" )
+    current_exit_code=$?
+    TOT_EXEC_TESTS=$((TOT_EXEC_TESTS+1))
+    if [ ${current_exit_code} -ne 0 ]; then
+        FAILED_TESTS+=("${NODE}:${TESTNAME}")
+        TOT_FAIL_TESTS=$((TOT_FAIL_TESTS+1))
+        EXIT_STATUS=1
+    fi
+done
+
+# Print a fancy table summarizing the test suit run
+echo -e "\n\n\n"
+echo -e "┌────────────────────────────────────────────────────────────────────────┐"
+echo -e "│                              ${TXT_BIYLW}TEST SUMMARY${TXT_NORML}                              │"
+echo -e "├────────────────────────────────────────────────────────────────────────┤"
+printf  "│ ${TXT_BIBLK} Total tests executed:  ${TXT_BIBLU} %3d ${TXT_NORML}                                          │\n" "${TOT_EXEC_TESTS}"
+PASSED_TESTS=$((TOT_EXEC_TESTS - TOT_FAIL_TESTS))
+printf  "│ ${TXT_BIBLK} Passed tests:          ${TXT_BIGRN} %3d ${TXT_NORML}                                          │\n" "${PASSED_TESTS}"
+printf  "│ ${TXT_BIBLK} Failed tests:          ${TXT_BIRED} %3d ${TXT_NORML}                                          │\n" "${TOT_FAIL_TESTS}"
+
+if [ ${TOT_FAIL_TESTS} -ne 0 ]; then
+    echo -e "├────────────────────────────────────────────────────────────────────────┤"
+    for failed_test in "${FAILED_TESTS[@]}"; do
+        printf "│     - %-64s │\n" "${failed_test}"
+    done
+fi
+echo -e "└────────────────────────────────────────────────────────────────────────┘"
+
+exit ${EXIT_STATUS}
